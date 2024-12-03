9fab266819925dcdb425a388771648e8766f0a75
BoonBunny <boon_suk@hotmail.com>
2024-12-03 23:05:11 +0700

    Fix Bug: Images did not work.

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -9,37 +9,37 @@ function Portfolio() {
       <div className={styles.port_list}>
         <div className={styles.port_items}>
             <Tilt scale={1.1} transitionSpeed={2500} tiltReverse={true}>
-              <img src="src/assets/Images/hospitality-services.webp" alt="coding" />
+              <img src="https://www.hirum.com.au/wp-content/uploads/2021/11/BookingForm-MillCottage-1536x882.png" alt="coding" />
             </Tilt>
             <p>Hotel & Hospitality Website </p>
         </div>
         <div className={styles.port_items}>
             <Tilt scale={1.1} transitionSpeed={2500} tiltReverse={true}>
-              <img src="src/assets/Images/ecommerce-service.webp" alt="coding" />
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRfywC-H7MfyE_AaN-vST3cgIRpjkvdvdRag&s" alt="coding" />
             </Tilt>
             <p>E-Commerce Website for electronic devices</p>
         </div>
         <div className={styles.port_items}>
             <Tilt scale={1.1} transitionSpeed={2500} tiltReverse={true}>
-              <img src="src/assets/Images/gearbox-service.webp" alt="coding" />
+              <img src="https://s3.envato.com/files/67658232/inline-preview/inline-preview.__large_preview.png" alt="coding" />
             </Tilt>
             <p>GearBox Website</p>
         </div>
         <div className={styles.port_items}>
             <Tilt scale={1.1} transitionSpeed={2500} tiltReverse={true}>
-              <img src="src/assets/Images/internal-website.webp" alt="coding" />
+              <img src="https://papyrs.com/site_media/images/docs/simple-internal-company-site-example2.png" alt="coding" />
             </Tilt>
             <p>Internal HR website</p>
         </div>
         <div className={styles.port_items}>
             <Tilt scale={1.1} transitionSpeed={2500} tiltReverse={true}>
-              <img src="src/assets/Images/online-banking.webp" alt="coding" />
+              <img src="https://agentestudio.com/uploads/ckeditor/pictures/1002/content_1_dashboard.png" alt="coding" />
             </Tilt>
             <p>Online e-banking System</p>
         </div>
         <div className={styles.port_items}>
             <Tilt scale={1.1} transitionSpeed={2500} tiltReverse={true}>
-              <img src="src/assets/Images/elearning-website.webp" alt="coding" />
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfudrrThVZdQdABl9amD-tDrlsagPpS-w9Ag&s" alt="coding" />
             </Tilt>
             <p>E-learning System Website</p>
         </div>
--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -7,20 +7,20 @@ function Testimonials() {
       <h3 className={styles.testi_title}>Testimonials</h3>
       <div className={styles.testi_list}>
         <div className={styles.testi_items}>
-            <p>Working with him was a game-changer for our website. They transformed our outdated interface into a sleek, modern design that’s both visually appealing and highly functional. Our users have complimented the seamless navigation, and our engagement metrics have improved dramatically. We couldn't be happier with the results!</p>
-            <img src="src/assets/Images/user1.png" alt="clients" />
+            <p>Working with him was a game-changer for our website. They transformed our outdated interface into a sleek, modern design that's both visually appealing and highly functional. Our users have complimented the seamless navigation, and our engagement metrics have improved dramatically. We couldn't be happier with the results!</p>
+            <img src="https://img.fixthephoto.com/blog/images/gallery/news_preview_mob_image__preview_11368.png" alt="clients" />
             <h4>Ananda Joe</h4>
             <p className={styles.bio}>Web Developer</p>
         </div>
         <div className={styles.testi_items}>
             <p>We hired him to revamp our e-commerce platform, and the results exceeded our expectations. Their attention to detail and expertise in responsive design ensured that our site looks fantastic on any device. Since the redesign, our mobile traffic and conversions have skyrocketed. Highly recommend their services!</p>
-            <img src="src/assets/Images/user2.png" alt="clients" />
+            <img src="https://media.istockphoto.com/id/1682296067/photo/happy-studio-portrait-or-professional-man-real-estate-agent-or-asian-businessman-smile-for.jpg?s=612x612&w=0&k=20&c=9zbG2-9fl741fbTWw5fNgcEEe4ll-JegrGlQQ6m54rg=" alt="clients" />
             <h4>Nirungul Insagul</h4>
             <p className={styles.bio}>Web Developer</p>
         </div>
         <div className={styles.testi_items}>
             <p>He brought incredible energy and creativity to our project. They were open to feedback, highly communicative, and consistently delivered beyond what we envisioned. Their innovative ideas and commitment to excellence made them an integral part of our project's success. We look forward to working with them again</p>
-            <img src="src/assets/Images/user3.png" alt="clients" />
+            <img src="https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTA5L3Jhd3BpeGVsX29mZmljZV8zNF9waG90b19vZl9hZnJpY2FuLWFtZXJpY2FuX3dvbWFuX2NvbXBhbnlfd29ya19kNmM4MmJhNS1iYjA2LTRkN2EtYjJlMy1hNDZhNDYyMjA0ZmZfMS5qcGc.jpg" />
             <h4>Benjamin Thavanahathaya</h4>
             <p className={styles.bio}>Web Developer</p>
         </div>
